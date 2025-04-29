a9f3d62b08d82ec8b0a8eeb82d02532e81a910bb
hakiers <mateusz.wojaczek@student.uj.edu.pl>
2025-04-29 22:48:35 +0200

    add tests

---
+++ backend/src/test/java/com/egradebook/backend/ControllerTests.java
@@ -0,0 +1,61 @@
+package com.egradebook.backend;
+
+import com.egradebook.backend.dto.UserLoginRequest;
+import com.egradebook.backend.model.LoginData;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ControllerTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    public ResponseEntity<String> login(String username, String password) throws Exception{
+        UserLoginRequest request = new UserLoginRequest(username, password);
+        ResponseEntity<String> response = restTemplate.postForEntity("/api/auth/login",
+                       request, String.class);
+        return response;
+    }
+
+    public ResponseEntity<String> logout() throws Exception{
+        ResponseEntity<String> response = restTemplate.postForEntity("/api/auth/logout", null, String.class);
+        return response;
+    }
+
+    @Test
+    public void loginAdmin() throws Exception {
+        ResponseEntity<String> response = login("admin", "admin");
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("admin", response.getBody());
+
+        assertEquals(HttpStatus.OK, logout().getStatusCode());
+    }
+
+    @Test
+    public void loginNotExistingUser() throws Exception {
+        ResponseEntity<String> response = login("nonExistentUser", "nonExistentUser");
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+        assertEquals("Invalid username or password", response.getBody());
+
+        assertEquals(HttpStatus.OK, logout().getStatusCode());
+    }
+
+    @Test
+    public void nonAdminAddStudent() throws Exception {
+
+
+        assertEquals(HttpStatus.OK, logout().getStatusCode());
+    }
+}
