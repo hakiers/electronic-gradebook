0ebfbb55c3ff2c19212f1689ea5f282901909fcd
hakiers <mateusz.wojaczek@student.uj.edu.pl>
2025-04-29 15:26:46 +0200

    update StudentRegistrationRequest

--- backend/src/main/java/com/egradebook/backend/dto/StudentRegistrationRequest.java
+++ backend/src/main/java/com/egradebook/backend/dto/StudentRegistrationRequest.java
@@ -4,17 +4,17 @@ public class StudentRegistrationRequest {
     private final String name;
     private final String surname;
     private final String pesel;
-    private final String class_id;
+    private final Integer class_id;
 
-    public StudentRegistrationRequest(String name, String surname, String pesel, String classId) {
+    public StudentRegistrationRequest(String name, String surname, String pesel, Integer classId) {
         this.name = name;
         this.surname = surname;
         this.pesel = pesel;
-        class_id = classId;
+        this.class_id = classId;
     }
 
     public String getName() { return name; }
     public String getSurname() { return surname; }
     public String getPesel() { return pesel; }
-    public String getClassId() { return class_id; }
+    public Integer getClassId() { return class_id; }
 }
