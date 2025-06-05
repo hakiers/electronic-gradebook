6d9777480c80a1f83f5bb49cf4bedd5151a57471
Olek-Wieczorek <aleksander.wieczorek@student.uj.edu.pl>
2025-06-05 13:52:52 +0200

    teacherStyles

--- frontend/src/main/java/com/egradebook/frontend/service/TeacherService.java
+++ frontend/src/main/java/com/egradebook/frontend/service/TeacherService.java
@@ -3,7 +3,6 @@ package com.egradebook.frontend.service;
 import com.egradebook.frontend.model.*;
 import com.egradebook.frontend.request.AddAttendanceRequest;
 import com.egradebook.frontend.request.EditAttendanceRequest;
-import com.egradebook.frontend.utils.StudentConverter;
 import com.egradebook.frontend.utils.Triple;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -63,12 +62,19 @@ public class TeacherService {
 
             HttpResponse<String> response = UserService.client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            List<Student> lista = StudentConverter.jsonToStudentList(response.body());
-            return new Pair<>(response.statusCode(), lista);
+            if (response.statusCode() == 200) {
+                List<Student> lista = mapper.readValue(
+                        response.body(),
+                        new TypeReference<List<Student>>() {
+                        }
+                );
+                return new Pair<>(response.statusCode(), lista);
+            }
         } catch (Exception e) {
             e.printStackTrace();
             return new Pair<>(500, null);
         }
+        return null;
     }
 
     public static Pair<Integer, List<Grade>> getGradesForStudent(int student_id, int subject_id) {
@@ -338,6 +344,36 @@ public class TeacherService {
                         new TypeReference<List<Teacher>>() {}
                 );
                 return new Pair<>(response.statusCode(), teachers);
+            }
+            else {
+                return new Pair<>(response.statusCode(), null);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Pair<>(500, null);
+        }
+    }
+    public static Pair<Integer, List<Subject>> getTeacherSubjects(int teacher_id) {
+        try {
+            if (UserService.getCurrentUsername() == null || UserService.getCurrentRole() == null) {
+                return new Pair<>(401, null);
+            }
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("http://localhost:8080/api/teacher/"+teacher_id+"/subjects"))
+                    .header("Content-Type", "application/json")
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = UserService.client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if(response.statusCode() == 200) {
+                List<Subject> subjects = mapper.readValue(
+                        response.body(),
+                        new TypeReference<List<Subject>>() {}
+                );
+                return new Pair<>(response.statusCode(), subjects);
             }
             else {
                 return new Pair<>(response.statusCode(), null);
